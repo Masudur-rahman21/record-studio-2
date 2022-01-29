65194e84d041914939d356b6869d26b493fbdb39
Masudur-rahman21 <93135613+Masudur-rahman21@users.noreply.github.com>
2022-01-29 13:56:05 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function menuBtn() {
+  window.scrollTo(0, 1000);
+}
+
+function aboutBtn() {
+  window.scrollTo(0, 500);
+}
+
+function contactBtn() {
+  window.scrollTo(0, 5000);
+}
+
+function readBtn() {
+  window.scrollTo(0, 5000);
+}
